f703dd926afb25c1df6664c5d067e28467eb972f
Moses Morris <56928217+Moses-Morris@users.noreply.github.com>
2023-05-02 01:17:56 +0300

    Create a default init file.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Initialize Blueprint views"""
+"""Initialize Blueprint views : App Blueprint."""
 from flask import Blueprint
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
